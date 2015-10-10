726d5d8e6b5c84210a31195d2141d37f77337e55
xuwei-k <6b656e6a69@gmail.com>
2015-10-10 15:05:25 +0900

    performance test

--- examples/src/main/scala/HelloWorld.scala
+++ examples/src/main/scala/HelloWorld.scala
@@ -1,22 +1,51 @@
 package io.grpc.scala
 package examples
 
-import scala.concurrent.ExecutionContext.Implicits.global
+import java.util.concurrent.atomic.AtomicInteger
+import java.util.concurrent.{CountDownLatch, TimeUnit}
+
+import com.google.common.util.concurrent.{FutureCallback, Futures}
+import io.grpc.examples.helloworld.{GreeterGrpc, HelloRequest, HelloResponse}
 import io.grpc.stub.StreamObserver
 
-import io.grpc.examples.helloworld.{GreeterGrpc, HelloRequest, HelloResponse}
+import scala.concurrent.Await
+import scala.concurrent.duration._
 
 object HelloWorldClient {
   def main(args: Array[String]) {
+    val n = args.headOption.fold(1)(_.toInt)
     val channel = Channel("localhost", 50051)
     val stub = GreeterGrpc.newFutureStub(channel)
     val request = HelloRequest.newBuilder().setName(args.headOption.getOrElse("Giskard")).build()
 
-    stub.sayHello(request).asFuture.map { response =>
-      println(response.getMessage)
-    }.onComplete { _ =>
-      channel.shutdown()//.awaitTerminated(5, TimeUnit.SECONDS)
+    Await.result(stub.sayHello(request).asFuture, 3.seconds)
+
+    val latch = new CountDownLatch(n)
+    val failures = new AtomicInteger()
+
+    val start = System.currentTimeMillis
+
+    var i = 0
+    while(i < n){
+      Futures.addCallback(stub.sayHello(request), new FutureCallback[HelloResponse] {
+        override def onFailure(t: Throwable): Unit = {
+          failures.incrementAndGet()
+          latch.countDown()
+        }
+        override def onSuccess(result: HelloResponse): Unit = {
+          latch.countDown()
+        }
+      })
+      i += 1
     }
+
+    latch.await(5000, TimeUnit.MILLISECONDS)
+    val time = System.currentTimeMillis() - start
+    println("failures " + failures.get)
+    println(time / 1000.0)
+    println((n / (time / 1000.0)) + " QPS")
+    channel.shutdown()
+    channel.awaitTerminated(10, TimeUnit.SECONDS)
   }
 }
 
